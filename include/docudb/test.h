a0d4dbb545eb33d7af105acb81b4ce2b8487f72e
Carson Howard <cjhoward92@gmail.com>
2022-01-02 22:30:31 -0700

    Initial commit

---
+++ include/docudb/test.h
@@ -0,0 +1,14 @@
+//
+// Created by carson on 1/2/22.
+//
+
+#ifndef DOCUDB_TEST_H
+#define DOCUDB_TEST_H
+
+#include <iostream>
+
+void Call() {
+    std::cout << "Calling..." << std::endl;
+}
+
+#endif //DOCUDB_TEST_H
